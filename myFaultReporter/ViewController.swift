d524b034f9205294c7e3f82091f7200f101bc62b
Henry McC <henrymcc@gmail.com>
2017-03-30 22:31:23 +0200

    Initial Commit

---
+++ myFaultReporter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myFaultReporter
+//
+//  Created by Henry McC on 2017/03/30.
+//  Copyright © 2017 HiddenPlatform. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
